be5f69c34cf340e22753290a8778b41418674ed8
sohamsawant3143-max <sohamsawant3143@gmail.com>
2026-07-14 10:11:03 +0530

    Add files via upload

---
+++ Prac1.java
@@ -0,0 +1,20 @@
+// import java.util.Scanner;
+public class Prac1{
+    public static void main(String[] args){
+        System.out.println("Bye world");
+        //implicit Casting
+        int a=45;
+        float b=a;
+        System.out.println(b);
+
+
+        //Explicit Casting
+        float marks=92.55f;
+        int Tmarks=(int)marks;
+        System.out.println(Tmarks);
+
+    
+
+    }
+
+}
