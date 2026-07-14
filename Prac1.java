0c208b5e393b2bfd3067ebee2690fcf9803a41ee
Soham Sawant <sohamsawant3143@gmail.com>
2026-07-14 10:17:42 +0530

    Enhance Prac1 with additional data types and output
    Added new data type variables and their outputs.

--- Prac1.java
+++ Prac1.java
@@ -2,6 +2,22 @@
 public class Prac1{
     public static void main(String[] args){
         System.out.println("Bye world");
+
+        //Data types
+        int age=20;
+        double salary=10000000.25;
+        char grade='A';
+        boolean isStudent=true;
+        string name="Soham";
+
+        System.out.println("Name: "+name);
+        System.out.println("Salary: "+salary);
+        System.out.println("Age: "+age);
+        System.out.println("Grade: "+grade);
+        System.out.println("Student: "+isStudent);
+
+
+        
         //implicit Casting
         int a=45;
         float b=a;
