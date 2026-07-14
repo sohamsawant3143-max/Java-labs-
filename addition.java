e71c0dd5cfbeedf0453005403f686f48cd5f024c
Soham Sawant <sohamsawant3143@gmail.com>
2026-07-14 11:09:35 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,12 @@
+public class addition {
+    public static void main(String[] args){
+         //Addition of Two Numbers:
+         int number1=10;
+         int number2=20;
+         int add=number1+number2;
+         System.out.println("Addition of " +number1+ " And "+number2+ " is: "+add);
+
+    }
+
+    
+}
